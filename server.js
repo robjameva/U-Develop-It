b6fec5b88db2e613642b3bf4b6c24cbe4ff94fb5
Robert Evanik <rob.evanik@gmail.com>
2022-01-10 16:52:00 -0500

    Connected to the db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,7 +8,16 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'robert.123',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
 
 
 
